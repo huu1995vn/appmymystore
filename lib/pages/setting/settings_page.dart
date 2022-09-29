6e255da0b7dbf8deef84e81cfe815048b3e6c367
tronghuu.dailyxe@outlook.com <tronghuu@dailyxe.com.vn>
2022-09-29 11:56:58 +0700

    ok

--- lib/pages/setting/settings_page.dart
+++ lib/pages/setting/settings_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:raoxe/app_icons.dart';
 import 'package:raoxe/core/commons/common_methods.dart';
@@ -87,103 +88,100 @@ class _SettingsPageState extends State<SettingsPage> {
       backgroundColor: Colors.transparent,
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverAppBar( 
-            title: Text('setting'.tr()), 
-            centerTitle: true, 
+          SliverAppBar(
+            title: Text('setting'.tr()),
+            centerTitle: true,
             elevation: 0.0,
           ),
           SliverFillRemaining(
-              child:   Column(
-                        children: [
-                          Expanded(
-                              child: ListView(
-                            shrinkWrap: true,
-                            children: [
-                              RxBuildItem(
-                                  icon: Icon(AppIcons.sun),
-                                  title: "Dark mode",
-                                  trailing: Switch(
-                                    value:
-                                        theme.selectedThemeMode.name == "dark",
-                                    onChanged: (value) {
-                                      theme.enableDarkMode(value);
-                                    },
-                                    activeTrackColor: Colors.red[200],
-                                    activeColor: Colors.red,
-                                  ),
-                                  onTap: () {
-                                    // _authenticateWithBiometrics();
-                                  }),
-                              RxBuildItem(
-                                icon: const Icon(AppIcons.text_format),
-                                title: context.locale.languageCode == "vi"
-                                    ? "Việt Nam"
-                                    : "English",
-                                trailing: Switch(
-                                  value: context.locale.languageCode != "vi",
-                                  onChanged: (value) {
-                                    context.setLocale(value
-                                        ? const Locale("en")
-                                        : const Locale("vi"));
-                                  },
-                                  activeTrackColor: Colors.red[200],
-                                  activeColor: Colors.red,
-                                ),
-                              ),
-                              RxBuildItem(
-                                  icon: const Icon(AppIcons.fingerprint),
-                                  title: "Đăng nhập bằng sinh trắc học",
-                                  trailing: Switch(
-                                    value: authBiometric,
-                                    onChanged: _onBiometric,
-                                    activeTrackColor: Colors.red[200],
-                                    activeColor: Colors.red,
-                                  ),
-                                  onTap: () {
-                                    // _authenticateWithBiometrics();
-                                  }),
-                              // RxBuildItem(
-                              //     title: "Clear cache".tr(),
-                              //     onTap: () {
-                              //       RxSearchDelegate.cacheapiSearch = {};
-                              //       CommonMethods.showToast(
-                              //           context, "success".tr());
-                              //     }),
-                              RxBuildItem(
-                                  icon: const Icon(AppIcons.share_1),
-                                  title: "share".tr(),
-                                  trailing: Icon(AppIcons.keyboard_arrow_right),
-                                  onTap: _onShare),
-                              RxBuildItem(
-                                  title: "termsandcondition".tr(),
-                                  onTap: () {
-                                    CommonMethods.openWebViewTermsAndCondition(
-                                        context);
-                                  }),
-                              RxBuildItem(
-                                  title: "policy".tr(),
-                                  onTap: () {
-                                    CommonMethods.openWebViewPolicy(context);
-                                  }),
-                              RxBuildItem(
-                                  title: "feedback".tr(),
-                                  onTap: () {
-                                    CommonMethods.openWebViewFeedBack(context);
-                                  }),
-                            ],
-                          )),
-                          RxRoundedButton(
-                            title: 'logout'.tr(),
-                            onPressed: () {
-                              AuthService.logout(context);
-                            },
-                          ),
-                          Text(
-                            "${"version".tr()} ${InfoDeviceService.infoDevice.PackageInfo?.version.toLowerCase()}",
-                            style: TextStyle().italic,
-                          )
-                        ],
-                      ))
+              child: Column(
+            children: [
+              Expanded(
+                  child: ListView(
+                shrinkWrap: true,
+                children: [
+                  RxBuildItem(
+                      icon: FaIcon(FontAwesomeIcons.sun),
+                      title: "Dark mode",
+                      trailing: Switch(
+                        value: theme.selectedThemeMode.name == "dark",
+                        onChanged: (value) {
+                          theme.enableDarkMode(value);
+                        },
+                        activeTrackColor: Colors.red[200],
+                        activeColor: Colors.red,
+                      ),
+                      onTap: () {
+                        // _authenticateWithBiometrics();
+                      }),
+                  RxBuildItem(
+                    icon: const Icon(AppIcons.text_format),
+                    title: context.locale.languageCode == "vi"
+                        ? "Việt Nam"
+                        : "English",
+                    trailing: Switch(
+                      value: context.locale.languageCode != "vi",
+                      onChanged: (value) {
+                        context.setLocale(
+                            value ? const Locale("en") : const Locale("vi"));
+                      },
+                      activeTrackColor: Colors.red[200],
+                      activeColor: Colors.red,
+                    ),
+                  ),
+                  RxBuildItem(
+                      icon: const Icon(AppIcons.fingerprint),
+                      title: "Đăng nhập bằng sinh trắc học",
+                      trailing: Switch(
+                        value: authBiometric,
+                        onChanged: _onBiometric,
+                        activeTrackColor: Colors.red[200],
+                        activeColor: Colors.red,
+                      ),
+                      onTap: () {
+                        // _authenticateWithBiometrics();
+                      }),
+                  // RxBuildItem(
+                  //     title: "Clear cache".tr(),
+                  //     onTap: () {
+                  //       RxSearchDelegate.cacheapiSearch = {};
+                  //       CommonMethods.showToast(
+                  //           context, "success".tr());
+                  //     }),
+                  RxBuildItem(
+                      icon: const Icon(AppIcons.share_1),
+                      title: "share".tr(),
+                      trailing: Icon(AppIcons.keyboard_arrow_right),
+                      onTap: _onShare),
+                  RxBuildItem(
+                      title: "termsandcondition".tr(),
+                      onTap: () {
+                        CommonMethods.openWebViewTermsAndCondition(context);
+                      }),
+                  RxBuildItem(
+                      title: "policy".tr(),
+                      onTap: () {
+                        CommonMethods.openWebViewPolicy(context);
+                      }),
+                  RxBuildItem(
+                      title: "feedback".tr(),
+                      onTap: () {
+                        CommonMethods.openWebViewFeedBack(context);
+                      }),
+                ],
+              )),
+              RxRoundedButton(
+                title: 'logout'.tr(),
+                onPressed: () {
+                  AuthService.logout(context);
+                },
+              ),
+              Text(
+                "${"version".tr()} ${InfoDeviceService.infoDevice.PackageInfo?.version.toLowerCase()}",
+                style: TextStyle().italic,
+              )
+            ],
+          ))
         ],
       ),
     );

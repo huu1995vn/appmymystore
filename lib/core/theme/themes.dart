b2323fe4aefbc9add5810725521d987311473f25
tronghuu.dailyxe <tronghuu.dailyxe@outlook.com>
2022-10-04 09:47:54 +0700

    ok

--- lib/core/theme/themes.dart
+++ lib/core/theme/themes.dart
@@ -4,51 +4,81 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:raoxe/core/utilities/app_colors.dart';
 
 class Themes {
-  
-  final lightTheme = ThemeData.light().copyWith(
+  static Color primaryColor = AppColors.primary;
+  static Color primaryColorDarkMode = AppColors.blackLight;
+  static ThemeData lightTheme = ThemeData.light().copyWith(
     inputDecorationTheme: const InputDecorationTheme(
       border: InputBorder.none,
     ),
-    primaryColor: AppColors.primary,
+    brightness: Brightness.light,
+    primaryColor: primaryColor,
     scaffoldBackgroundColor: AppColors.grey,
     cardColor: AppColors.white,
     dividerColor: AppColors.black.withOpacity(0.1),
     shadowColor: AppColors.grayDark,
-    // primarySwatch: getMaterialColorFromColor(primaryColor),
-    textTheme: GoogleFonts.latoTextTheme(
-        ThemeData(brightness: Brightness.light).textTheme.copyWith(
-              bodyText1: const TextStyle(fontSize: 16.0),
-              bodyText2: const TextStyle(fontSize: 13.0),
-              button: const TextStyle(fontSize: 13.0),
-            )),
-    iconTheme: const IconThemeData(size: 19, color: AppColors.blackLight),
-    appBarTheme: const AppBarTheme(
-      elevation: 0,
-      backgroundColor: AppColors.primary,
-      systemOverlayStyle: SystemUiOverlayStyle.light,
-    ),
+    
+    // textTheme: GoogleFonts.latoTextTheme(ThemeData(brightness:  Brightness.light).textTheme.copyWith(
+    //       bodyLarge: const TextStyle(fontSize: 16.0),
+    //       bodyMedium: const TextStyle(fontSize: 13.0),
+    //       labelLarge: const TextStyle(fontSize: 16.0),
+    //     )),
+    iconTheme: const IconThemeData(size: 19, color: AppColors.white),
+    appBarTheme: AppBarTheme(
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: AppColors.white,
+        ),
+        backgroundColor: primaryColor,
+        systemOverlayStyle: SystemUiOverlayStyle.light),
   );
-  final darkTheme = ThemeData.dark().copyWith(
+  static ThemeData darkTheme = ThemeData.dark().copyWith(
     inputDecorationTheme: const InputDecorationTheme(
       border: InputBorder.none,
     ),
-    brightness: Brightness.dark,
-    primaryColor: AppColors.black,
-    scaffoldBackgroundColor: Colors.black,
-    cardColor: AppColors.blackLight,
-    dividerColor: AppColors.white.withOpacity(0.2),
-    shadowColor: AppColors.text,
-    textTheme: GoogleFonts.latoTextTheme(
-        ThemeData(brightness: Brightness.dark).textTheme.copyWith(
-              bodyText1: const TextStyle(fontSize: 16.0),
-              bodyText2: const TextStyle(fontSize: 13.0),
-              button: const TextStyle(fontSize: 13.0),
-            )),
-    iconTheme: const IconThemeData(size: 19, color: AppColors.white),
-    appBarTheme: const AppBarTheme(
-      elevation: 0,
-      backgroundColor: AppColors.primary,
-      systemOverlayStyle: SystemUiOverlayStyle.dark,
-    ),
+    // brightness: Brightness.dark,
+    // primaryColor: AppColors.white,
+    // scaffoldBackgroundColor: Colors.black,
+    // cardColor: AppColors.blackLight,
+    // dividerColor: AppColors.white.withOpacity(0.2),
+    // shadowColor: AppColors.text,
+    textTheme: GoogleFonts.latoTextTheme(ThemeData(brightness: Brightness.dark).textTheme.copyWith(
+          bodyLarge: const TextStyle(fontSize: 16.0, color: AppColors.black),
+          bodyMedium: const TextStyle(fontSize: 13.0, color: AppColors.black),
+          labelLarge: const TextStyle(fontSize: 16.0, color: AppColors.black),
+        )),
+    iconTheme: const IconThemeData(size: 19),
+    appBarTheme: AppBarTheme(
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: AppColors.white,
+        ),
+        backgroundColor: primaryColorDarkMode,
+        systemOverlayStyle: SystemUiOverlayStyle.light),
   );
+  // static Color getShade(Color color, {bool darker = false, double value = .1}) {
+  //   assert(value >= 0 && value <= 1);
+
+  //   final hsl = HSLColor.fromColor(color);
+  //   final hslDark = hsl.withLightness(
+  //       (darker ? (hsl.lightness - value) : (hsl.lightness + value))
+  //           .clamp(0.0, 1.0));
+
+  //   return hslDark.toColor();
+  // }
+
+  // static MaterialColor getMaterialColorFromColor(Color color) {
+  //   Map<int, Color> colorShades = {
+  //     50: getShade(color, value: 0.5),
+  //     100: getShade(color, value: 0.4),
+  //     200: getShade(color, value: 0.3),
+  //     300: getShade(color, value: 0.2),
+  //     400: getShade(color, value: 0.1),
+  //     500: color, //Primary value
+  //     600: getShade(color, value: 0.1, darker: true),
+  //     700: getShade(color, value: 0.15, darker: true),
+  //     800: getShade(color, value: 0.2, darker: true),
+  //     900: getShade(color, value: 0.25, darker: true),
+  //   };
+  //   return MaterialColor(color.value, colorShades);
+  // }
 }

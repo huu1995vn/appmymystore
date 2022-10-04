fddf2c702902cd4c06a1e586c62b7906417aac68
tronghuu.dailyxe <tronghuu.dailyxe@outlook.com>
2022-10-04 14:29:31 +0700

    ok

--- lib/pages/product/widgets/list_brand.widget.dart
+++ lib/pages/product/widgets/list_brand.widget.dart
@@ -26,43 +26,45 @@ class _ListBrandWidgetState extends State<ListBrandWidget>
   Widget build(BuildContext context) {
     super.build(context);
     return SizedBox(
-      height: 76,
+      height: 75,
       child: RxListView(
         data,
         (context, index) {
           var item = data[index];
           return Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding),
+            padding: const EdgeInsets.only(right: kDefaultPadding),
             child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
-                  side: BorderSide(
-                      width: 1,
-                      color: widget.value == item["id"]
-                          ? AppColors.primary
-                          : Colors.black26)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 60,
-                    height: 30,
-                    child: RxImage(
-                      CommonMethods.buildUrlHinhDaiDien(item["img"]),
-                      width: 50,
-                      height: 30,
-                    ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                    side: BorderSide(
+                        width: 1,
+                        color: widget.value == item["id"]
+                            ? AppColors.primary
+                            : Colors.black26)),
+                child: SizedBox(
+                  height: 65,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 60,
+                        height: 30,
+                        child: RxImage(
+                          CommonMethods.buildUrlHinhDaiDien(item["img"]),
+                        
+                        ),
+                      ),
+                      Text(
+                        item["name"],
+                        style: TextStyle(fontSize: 10),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    ],
                   ),
-                  Text(
-                    item["name"],
-                    style: TextStyle(fontSize: 10),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  )
-                ],
+                ),
               ),
-            ),
           );
         },
         noFound: Container(),

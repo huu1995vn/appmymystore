63b5ac1905e2ac15864755fe0b5babf8055f0955
tronghuu.dailyxe <tronghuu.dailyxe@outlook.com>
2022-10-04 12:47:58 +0700

    ok

--- lib/pages/main/news/news_page.dart
+++ lib/pages/main/news/news_page.dart
@@ -72,29 +72,6 @@ class _NewsPageState extends State<NewsPage> {
         ),
       ),
     );
-    // DefaultTabController(
-    //     length: tabs.length,
-    //     child: Scaffold(
-    //         appBar: AppBar(
-    //             centerTitle: true,
-    //             title: Text("news".tr),
-    //             automaticallyImplyLeading: false,
-    //             elevation: 0.0,
-    //             bottom: PreferredSize(
-    //                 preferredSize: Size.fromHeight(50),
-    //                 child: ColoredBox(
-    //                 color: Get.isDarkMode ? Colors.white10 : Colors.white,
-    //                   child: TabBar(
-    //                    isScrollable: true,
-    //                   labelColor: AppColors.primary,
-    //                   unselectedLabelColor:
-    //                       Get.isDarkMode ? Colors.white : AppColors.black,
-    //                   indicatorColor: Colors.red[800],
-    //                   tabs: tabs,
-    //                   ),
-    //                 ))),
-    //         body: TabBarView(
-    //           children: tabviews,
-    //         )));
+   
   }
 }

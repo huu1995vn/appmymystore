66f3704b0df2cb7872db050453192d2797d9eaf0
tronghuu.dailyxe <tronghuu.dailyxe@outlook.com>
2022-10-20 11:04:36 +0700

    ok

--- lib/core/commons/common_methods.dart
+++ lib/core/commons/common_methods.dart
@@ -84,10 +84,10 @@ class CommonMethods {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.deniedForever) {
-        return Future.error('Location Not Available');
+        return null;
       }
     } else {
-      return null;
+        return null;
     }
     return await Geolocator.getCurrentPosition();
   }
